d1cc9804c398d972be815b5fc8d0fe1da58e8ca7
silentleewei <33079326@qq.com>
2022-04-16 17:28:53 +0800

    hotfix  6.1

--- shiro-springboot/src/main/java/com/kuang/util/test.java
+++ shiro-springboot/src/main/java/com/kuang/util/test.java
@@ -14,5 +14,6 @@ public class test {
         System.out.println("hotfix 3");
         System.out.println("master 4.1");
         System.out.println("hotfix 5.1");
+        System.out.println("hotfix 6.1");
     }
 }
