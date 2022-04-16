26236a7b7671cc575b60c12be2f2787a61354cd6
silentleewei <33079326@qq.com>
2022-04-16 17:48:12 +0800

    hotfix  8.1

--- shiro-springboot/src/main/java/com/kuang/util/test.java
+++ shiro-springboot/src/main/java/com/kuang/util/test.java
@@ -20,5 +20,6 @@ public class test {
         System.out.println("hotfix 5.1");
         System.out.println("hotfix 6.1");
         System.out.println("hotfix 7.1");
+        System.out.println("hotfix 8.1");
     }
 }
