52ee14371c93c460a8fc36ffdd438d58c7cb0f7f
silentleewei <33079326@qq.com>
2022-04-16 16:23:41 +0800

    2 version

---
+++ shiro-springboot/src/main/java/com/kuang/util/test.java
@@ -0,0 +1,10 @@
+package com.kuang.util;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("leewei1");
+        System.out.println("leewei1");
+        System.out.println("leewei1");
+        System.out.println("leewei1");
+    }
+}
