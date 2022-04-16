add17d15734d8a09430b388a8cda86f74c91c4ae
silentleewei <33079326@qq.com>
2022-04-16 16:21:46 +0800

    first version

---
+++ shiro-springboot/src/main/java/com/kuang/util/test.java
@@ -0,0 +1,10 @@
+package com.kuang.util;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello git1");
+        System.out.println("hello git2");
+        System.out.println("hello git3");
+        System.out.println("hello git4");
+    }
+}
